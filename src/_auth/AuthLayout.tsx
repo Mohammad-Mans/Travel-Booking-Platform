df27057ac68464e75aabd0514c627715d464a594
Mohammad Mansour <mohammed.i02@hotmail.com>
2024-02-27 00:16:31 +0200

    Change AuthLayout page background image

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import "./AuthLayout.css";
 import { Grid, Typography } from "@mui/material";
+import backgroundImage from "../assets/images/vista-voyage-login.png";
 
 const AuthLayout = () => {
   return (
@@ -10,6 +10,14 @@ const AuthLayout = () => {
       className="auth"
       justifyContent="center"
       height="100vh"
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
+        boxShadow: "inset 0 0 0 1000px #00000055",
+      }}
     >
       <Grid
         item
