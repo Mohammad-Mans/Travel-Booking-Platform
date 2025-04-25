cba0e58532314ece7a8c760fa3a0aa27100cca4b
Mohammad Mansour <mohammed.i02@hotmail.com>
2025-04-26 00:58:39 +0300

    Change hotel card name

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -24,7 +24,7 @@ import { useSearchParams } from "react-router-dom";
 import ResponsiveColoredGrid from "../../components/common/ResponsiveColoredGrid";
 import SearchBar from "../../components/features/SearchBar/SearchBar";
 import { useEffect, useState } from "react";
-import HotelCard from "./components/HotelCard";
+import HotelCard from "./components/HotelCardTMP";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
